f25c8b98415a6ad64f2fdc076d5ad687a9003c3e
Mark Cheltsov <chinaboygang@gmail.com>
2023-09-29 13:35:07 +0300

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import CreateEvent from './pages/create-event/create-event';
 
 function App() {
   return (
     <div className="App">
+      <CreateEvent/>
     </div>
   );
 }
---
+++ src/pages/create-event/create-event.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateEvent() {
+  return (
+    <div>
+        <p>создать ивент</p>
+    </div>
+  )
+}
+
+export default CreateEvent
