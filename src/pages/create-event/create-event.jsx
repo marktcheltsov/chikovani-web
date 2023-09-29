a50353d2080695be4d43eecec886473434f52689
Mark Cheltsov <chinaboygang@gmail.com>
2023-09-29 17:20:43 +0300

    start creating event form

--- src/pages/create-event/create-event.jsx
+++ src/pages/create-event/create-event.jsx
@@ -9,6 +9,12 @@ function CreateEvent() {
         <Input icon={MapPin} placeholder={'Address'}/>
         <Input icon={MessageSquare} placeholder={'Comment'}/>
         <div>
+            <Input icon={CalendarDays} placeholder={'Date'}/>
+            <Input icon={Clock10} placeholder={'Time'}/> 
+        </div>
+        <Input icon={SlidersHorizontal} placeholder={'Format'}/>
+        <Input icon={Users} placeholder={'Slots'}/>
+        <div>
             <DropdownMenu title='format' items={['3x3', '4x4']}/>
         </div>
     </div>
